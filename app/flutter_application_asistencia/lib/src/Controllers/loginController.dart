2c112240ac303b3c2487d57d6bcb866d502448da
Yael2924 <saulyaeld@gmail.com>
2025-05-12 11:45:38 -0600

    Dividir iniciodesesion
    Se acomodaron las funciones en el archivo que le correspondía loginController

--- app/flutter_application_asistencia/lib/src/Controllers/loginController.dart
+++ app/flutter_application_asistencia/lib/src/Controllers/loginController.dart
@@ -1,1 +1,65 @@
-//controladores para la funcioanalidad del archivo.
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:http/http.dart' as http;
+
+class LoginController {
+  final TextEditingController controllerUsuario = TextEditingController();
+  final TextEditingController controllerPassword = TextEditingController();
+
+  void dispose() {
+    controllerUsuario.dispose();
+    controllerPassword.dispose();
+  }
+
+  Future<void> verificarlogin(BuildContext context) async {
+    if (controllerUsuario.text.trim().isEmpty ||
+        controllerPassword.text.trim().isEmpty) {
+      mostrarmensaje(
+          context, 'ERROR', 'Por favor, ingresa usuario y contraseña', DialogType.error);
+      return;
+    }
+
+    final url = Uri.parse(
+        "http://localhost/Asistencia/api/usuariosapp.php?accion=login");
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: json.encode({
+          'nombre': controllerUsuario.text.trim(),
+          'clave': controllerPassword.text.trim(),
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+
+        if (data['success']) {
+          mostrarmensaje(context, 'Login', data['message'], DialogType.success);
+        } else {
+          mostrarmensaje(context, 'Login', data['message'], DialogType.error);
+        }
+      } else {
+        mostrarmensaje(context, 'Error',
+            'Error en la solicitud: ${response.statusCode}', DialogType.error);
+      }
+    } catch (e) {
+      mostrarmensaje(context, 'Error', 'Error al conectarse al servidor: $e',
+          DialogType.error);
+    }
+  }
+
+  void mostrarmensaje(
+      BuildContext context, String titulo, String mensaje, DialogType tipo) {
+    AwesomeDialog(
+      context: context,
+      dialogType: tipo,
+      animType: AnimType.bottomSlide,
+      title: titulo,
+      desc: mensaje,
+      btnOkOnPress: () {},
+    ).show();
+  }
+}
--- app/flutter_application_asistencia/lib/src/views/iniciodesesion.dart
+++ app/flutter_application_asistencia/lib/src/views/iniciodesesion.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_asistencia/src/Controllers/loginController.dart';
 import 'package:flutter_application_asistencia/src/temas/botones.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:awesome_dialog/awesome_dialog.dart';
+
 
 class Login extends StatefulWidget {
   const Login({super.key, required this.title});
@@ -13,14 +12,12 @@ class Login extends StatefulWidget {
 }
 
 class Iniciosesion extends State<Login> {
-  final TextEditingController controllerUsuario = TextEditingController();
-  final TextEditingController controllerPassword = TextEditingController();
+  final LoginController _loginController = LoginController();
   bool _obscureText = true;
 
   @override
   void dispose() {
-    controllerUsuario.dispose();
-    controllerPassword.dispose();
+    _loginController.dispose();
     super.dispose();
   }
 
@@ -65,7 +62,7 @@ class Iniciosesion extends State<Login> {
 
   Widget txtUsuario() {
     return TextFormField(
-      controller: controllerUsuario,
+      controller: _loginController.controllerUsuario,
       decoration: const InputDecoration(
         labelText: 'Usuario',
       ),
@@ -74,7 +71,7 @@ class Iniciosesion extends State<Login> {
 
   Widget txtPassword() {
     return TextFormField(
-      controller: controllerPassword,
+      controller: _loginController.controllerPassword,
       obscureText: _obscureText,
       decoration: InputDecoration(
         labelText: 'Contraseña',
@@ -94,70 +91,9 @@ class Iniciosesion extends State<Login> {
     );
   }
 
-  Future<void> verificarlogin() async {
-    //  Validación: campos vacíos
-    if (controllerUsuario.text.trim().isEmpty ||
-        controllerPassword.text.trim().isEmpty) {
-      mostrarmensaje(
-          'ERROR', 'Por favor, ingresa usuario y contraseña', DialogType.error);
-      return;
-    }
-    final url = Uri.parse(
-        "http://localhost/Asistencia/api/usuariosapp.php?accion=login");
-
-    try {
-      final response = await http.post(
-        url,
-        headers: {"Content-Type": "application/json"},
-        body: json.encode({
-          'nombre': controllerUsuario.text.trim(),
-          'clave': controllerPassword.text.trim(),
-        }),
-      );
-
-      if (response.statusCode == 200) {
-        // Si la solicitud fue exitosa
-        final data = json.decode(response.body);
-
-        if (data['success']) {
-          mostrarmensaje('Login', data['message'],
-              DialogType.success); // "Credenciales correctas"
-        } else {
-          mostrarmensaje('Login', data['message'],
-              DialogType.error); // "Credenciales incorrectas"
-        }
-      } else {
-        // Si la solicitud no fue exitosa, muestra un error
-        mostrarmensaje('Error', 'Error en la solicitud: ${response.statusCode}',
-            DialogType.error);
-      }
-    } catch (e) {
-      // Si hay un error en la conexión o en el formato de la solicitud
-      mostrarmensaje(
-          'Error', 'Error al conectarse al servidor: $e', DialogType.error);
-    }
-  }
-
   Widget botoningresar() {
-    return Estilosbotones.btnprimary('Ingresar', verificarlogin);
-  }
-
-/*
-  void showMessage(String message) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(message)),
-    );
-  }
-*/
-  void mostrarmensaje(String titulo, String mensaje, DialogType tipo) {
-    AwesomeDialog(
-      context: context,
-      dialogType: tipo,
-      animType: AnimType.bottomSlide,
-      title: titulo,
-      desc: mensaje,
-      btnOkOnPress: () {},
-    ).show();
+    return Estilosbotones.btnprimary(
+        'Ingresar', () => _loginController.verificarlogin(context));
   }
 
   Widget logo() {
