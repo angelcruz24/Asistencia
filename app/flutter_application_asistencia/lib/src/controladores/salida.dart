e3248ba64ad2d877355ddca595f9ce8ac3d913c3
Juangomez0122 <juandejesusgomezsanchez12@gmail.com>
2025-05-15 10:57:53 -0600

    Se agregaron los campos de BSSID y UUI al controlador de salida

--- app/flutter_application_asistencia/lib/src/controladores/salida.dart
+++ app/flutter_application_asistencia/lib/src/controladores/salida.dart
@@ -6,7 +6,8 @@ class salidacontroller {
   final TextEditingController fechasalidacontroller = TextEditingController(text: "2025-05-13");
   final TextEditingController horasalidacontroller = TextEditingController(text: "18:00");
   final TextEditingController ipsalidacontroller = TextEditingController(text: "192.168.1.10");
-  final TextEditingController macsalidacontroller = TextEditingController(text: "00:1A:2B:3C:4D:5E");
+  final TextEditingController bssidsalidadcontroller = TextEditingController(text: "00:1A:2B:3C:4D:5E");
+  final TextEditingController uuisalidacontroller = TextEditingController(text: "00:1A:2B:3C:4D:5E");
   final TextEditingController actividadescontroller = TextEditingController();
 
   void registrarSalida() {
@@ -16,7 +17,8 @@ class salidacontroller {
     debugPrint("Fecha: ${fechasalidacontroller.text}");
     debugPrint("Hora: ${horasalidacontroller.text}");
     debugPrint("IP: ${ipsalidacontroller.text}");
-    debugPrint("MAC: ${macsalidacontroller.text}");
+    debugPrint("BSSID: ${bssidsalidadcontroller.text}");
+    debugPrint("UUI: ${uuisalidacontroller.text}");
     debugPrint("Actividades: ${actividadescontroller.text}");
   }
 
@@ -25,7 +27,8 @@ class salidacontroller {
     fechasalidacontroller.dispose();
     horasalidacontroller.dispose();
     ipsalidacontroller.dispose();
-    macsalidacontroller.dispose();
+    bssidsalidadcontroller.dispose();
+    uuisalidacontroller.dispose();
     actividadescontroller.dispose();
   }
 }
